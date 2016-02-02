6881637cf30c83a1443d27772ccb7c93c543c2bb
stasj <stasj@dnb.com>
2016-02-02 10:56:44 +0000

    Add hello world controller

--- src/test/java/com/jakubstas/FoosieApplicationTests.java
+++ src/test/java/com/jakubstas/FoosieApplicationTests.java
@@ -1,18 +1,19 @@
 package com.jakubstas;
 
+import com.jakubstas.foosie.FoosieApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = FoosieApplication.class)
 @WebAppConfiguration
 public class FoosieApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
