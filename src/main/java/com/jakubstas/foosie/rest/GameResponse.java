3d2b61b6a1f89cd41946e89872be3580eea38230
Jakub Stas <jakub@jakubstas.com>
2016-02-03 23:07:06 +0000

    Add quick reply

---
+++ src/main/java/com/jakubstas/foosie/rest/GameResponse.java
@@ -0,0 +1,47 @@
+package com.jakubstas.foosie.rest;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class GameResponse {
+
+    @JsonProperty
+    private String text;
+
+    @JsonProperty
+    private Attachment[] attachments = new Attachment[1];
+
+    public GameResponse(String text) {
+        this.text = text;
+        attachments[0] = new Attachment();
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Attachment[] getAttachments() {
+        return attachments;
+    }
+
+    public void setAttachments(Attachment[] attachments) {
+        this.attachments = attachments;
+    }
+
+    class Attachment {
+
+        @JsonProperty
+        private String text;
+
+        public String getText() {
+            return text;
+        }
+
+        public void setText(String text) {
+            this.text = text;
+        }
+    }
+}
