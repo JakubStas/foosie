47ca0b012bd8ac7c0a47d84bff7c9b5690eea712
Jakub Stas <jakub@jakubstas.com>
2016-02-11 22:46:40 +0000

    Add game url validation test

--- src/main/java/com/jakubstas/foosie/validation/GameUrlValidator.java
+++ src/main/java/com/jakubstas/foosie/validation/GameUrlValidator.java
@@ -1,13 +1,13 @@
 package com.jakubstas.foosie.validation;
 
-import org.hibernate.validator.internal.constraintvalidators.hv.URLValidator;
+import org.apache.commons.validator.UrlValidator;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 public class GameUrlValidator implements ConstraintValidator<GameUrl, String> {
 
-    private final URLValidator urlValidator = new URLValidator();
+    private final UrlValidator urlValidator = new UrlValidator(new String[]{"https"});
 
     @Override
     public void initialize(GameUrl gameUrl) {
@@ -16,6 +16,6 @@ public class GameUrlValidator implements ConstraintValidator<GameUrl, String> {
 
     @Override
     public boolean isValid(String url, ConstraintValidatorContext constraintValidatorContext) {
-        return urlValidator.isValid(url, constraintValidatorContext);
+        return urlValidator.isValid(url);
     }
 }
