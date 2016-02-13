02529386052ce471afbe4005bb515149f5d36219
Jakub Stas <jakub@jakubstas.com>
2016-02-13 00:55:41 +0000

    Enable scheduling

--- src/main/java/com/jakubstas/foosie/configuration/ApplicationConfiguration.java
+++ src/main/java/com/jakubstas/foosie/configuration/ApplicationConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.client.RestTemplate;
@@ -14,6 +15,7 @@ import javax.validation.Validator;
 @Configuration
 @EnableAutoConfiguration
 @EnableAspectJAutoProxy
+@EnableScheduling
 @EnableConfigurationProperties({SlackProperties.class, FoosieProperties.class})
 public class ApplicationConfiguration {
 
