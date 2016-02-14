72bd7dd5e8981979c0dbc22c10bea1be02945ec3
Jakub Stas <jakub@jakubstas.com>
2016-02-14 17:53:47 +0000

    Add channel handling

--- src/main/java/com/jakubstas/foosie/slack/SlackService.java
+++ src/main/java/com/jakubstas/foosie/slack/SlackService.java
@@ -3,14 +3,17 @@ package com.jakubstas.foosie.slack;
 import com.jakubstas.foosie.configuration.SlackProperties;
 import com.jakubstas.foosie.rest.PrivateReply;
 import com.jakubstas.foosie.service.model.User;
+import com.jakubstas.foosie.slack.json.OpenChannelResponse;
 import com.jakubstas.foosie.slack.json.SlackMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.annotation.PostConstruct;
 import java.net.URI;
 
 @Service
@@ -18,13 +21,24 @@ public class SlackService {
 
     private final Logger logger = LoggerFactory.getLogger(SlackService.class);
 
+    private final String openPrivateMessageChannelPath = "https://slack.com/api/im.open";
+
     private final String postPrivateMessagePath = "https://slack.com/api/chat.postMessage";
+
+    private final String closePrivateMessageChannelPath = "https://slack.com/api/im.close";
 
     @Autowired
     private RestTemplate restTemplate;
 
     @Autowired
     private SlackProperties slackProperties;
+
+    @PostConstruct
+    public void setup() {
+        final String s = openPrivateMessageChannelAndReturnChannelId(new User("jakub", "U0LHQNY0M"));
+        postPrivateMessageToPlayerChannel("asdsad", s);
+        closePrivateMessageChannel(s);
+    }
 
     public void postMessageToChannel(final String message) {
         logger.info("Posting message: {}", message);
@@ -47,15 +61,48 @@ public class SlackService {
     }
 
     public void postPrivateMessageToPlayer(final User player, final String text) {
-        logger.info("Posting private message to user: {}", player.getUserName());
+        final String channelId = openPrivateMessageChannelAndReturnChannelId(player);
+        postPrivateMessageToPlayerChannel(text, channelId);
+        closePrivateMessageChannel(channelId);
+    }
 
-        final String uriAsString = UriComponentsBuilder.fromPath(postPrivateMessagePath).queryParam("token", slackProperties.getAuthToken()).queryParam("channel", player.getUserId()).queryParam("text", text).queryParam("pretty", 1).build(true).toString();
+    private String openPrivateMessageChannelAndReturnChannelId(final User player) {
+        logger.info("Opening private message channel to user: {}", player.getUserName());
+
+        final String uriAsString = UriComponentsBuilder.fromUriString(openPrivateMessageChannelPath).queryParam("token", slackProperties.getAuthToken()).queryParam("user", player.getUserId()).queryParam("pretty", 1).build(true).toString();
+
+        final URI uri = URI.create(uriAsString);
+
+        logger.info("URI = " + uri.toString());
+
+        final ResponseEntity<OpenChannelResponse> responseEntity = restTemplate.getForEntity(uri, OpenChannelResponse.class);
+        final OpenChannelResponse openChannelResponse = responseEntity.getBody();
+
+        return openChannelResponse.getChannel().getId();
+    }
+
+    private void postPrivateMessageToPlayerChannel(final String text, final String channelId) {
+        logger.info("Posting private message to user channel: {}", channelId);
+
+        final String uriAsString = UriComponentsBuilder.fromUriString(postPrivateMessagePath).queryParam("token", slackProperties.getAuthToken()).queryParam("channel", channelId).queryParam("text", text).queryParam("pretty", 1).build(true).toString();
 
         final URI uri = URI.create(uriAsString);
 
         logger.info("URI = " + uri.toString());
         logger.info("Message = " + text);
 
-        restTemplate.postForLocation(uri, null);
+        restTemplate.getForEntity(uri, null);
+    }
+
+    private void closePrivateMessageChannel(final String channelId) {
+        logger.info("Closing private message channel: {}", channelId);
+
+        final String uriAsString = UriComponentsBuilder.fromUriString(closePrivateMessageChannelPath).queryParam("token", slackProperties.getAuthToken()).queryParam("channel", channelId).queryParam("pretty", 1).build(true).toString();
+
+        final URI uri = URI.create(uriAsString);
+
+        logger.info("URI = " + uri.toString());
+
+        restTemplate.getForEntity(uri, null);
     }
 }
