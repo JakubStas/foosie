73c5c6cabde305338ad22d2d26b690a6629bb254
Jakub Stas <jakub@jakubstas.com>
2016-02-22 00:32:57 +0000

    WIP

--- src/main/java/com/jakubstas/foosie/service/message/MessageTemplates.java
+++ src/main/java/com/jakubstas/foosie/service/message/MessageTemplates.java
@@ -32,6 +32,18 @@ public final class MessageTemplates {
 
     private final static String successfullyJoinedGamePrivateMessageBody = "You have successfully joined game by %s starting at %s";
 
+    private final static String successfullyCancelledGamePrivateMessageBody = "Your game has been successfully cancelled!";
+
+    private final static String gameCancelledChannelMessageBody = "Lobby for %ss game has been closed. The game is cancelled!";
+
+    private final static String unableToCancelGamePrivateMessageBody = "You have no active games to be cancelled.";
+
+    private final static String successfullyRescheduledGamePrivateMessageBody = "Your game has been successfully rescheduled to %s!";
+
+    private final static String gameRescheduledChannelMessageBody = "%ss game has been rescheduled to %s";
+
+    private final static String unableToRescheduleGamePrivateMessageBody = "You have no active games to be rescheduled.";
+
     public static final String createInternalErrorPrivateMessageBody() {
         return internalErrorPrivateMessageBody;
     }
@@ -90,5 +102,29 @@ public final class MessageTemplates {
 
     public static final String createSuccessfullyJoinedGamePrivateMessageBody(final String hostName, final String proposedTime) {
         return String.format(successfullyJoinedGamePrivateMessageBody, hostName, proposedTime);
+    }
+
+    public static final String createSuccessfullyRescheduledGamePrivateMessageBody(final String hostName) {
+        return String.format(successfullyRescheduledGamePrivateMessageBody, hostName);
+    }
+
+    public static final String createGameCancelledChannelMessageBody(final String hostName) {
+        return String.format(gameCancelledChannelMessageBody, hostName);
+    }
+
+    public static final String createUnableToCancelGamePrivateMessageBody() {
+        return unableToCancelGamePrivateMessageBody;
+    }
+
+    public static final String createSuccessfullyCancelledGamePrivateMessageBody(final String proposedTime) {
+        return String.format(successfullyCancelledGamePrivateMessageBody, proposedTime);
+    }
+
+    public static final String createGameRescheduledChannelMessageBody(final String hostName, final String proposedTime) {
+        return String.format(gameRescheduledChannelMessageBody, hostName, proposedTime);
+    }
+
+    public static final String createUnableToRescheduleGamePrivateMessageBody() {
+        return unableToRescheduleGamePrivateMessageBody;
     }
 }
